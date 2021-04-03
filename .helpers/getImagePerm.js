3ba76727f1b7238fc70f3f12e1732a13ba97a1b8
Nathan Galley <Nathan-Galley@users.noreply.github.com>
2021-04-03 17:45:59 -0600

    Added Many Things

---
+++ .helpers/getImagePerm.js
@@ -0,0 +1,2 @@
+const alg = "(M2' U M2' U) (M' U2) (M2' U2 M') [U2]"
+console.log("http://cube.rider.biz/visualcube.php?&view=plan&fmt=png&bg=t&size=1024&case="+encodeURIComponent(alg.replace(/( |\(|\))/g, '')))
