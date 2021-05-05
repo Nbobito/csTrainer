912b10ab47825b6ba278757f20fee6fa5f4844ce
Nbobito <nbobito12@gmail.com>
2021-05-05 08:07:02 -0600

    Downloaded many files

--- .helpers/getImagePerm.js
+++ .helpers/getImagePerm.js
@@ -1,2 +1,10 @@
-const alg = "(M2' U M2' U) (M' U2) (M2' U2 M') [U2]"
-console.log("http://cube.rider.biz/visualcube.php?&view=plan&fmt=png&bg=t&size=1024&case="+encodeURIComponent(alg.replace(/( |\(|\))/g, '')))
+const open = require('open')
+const read = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+read.question('Input alg: ', alg=>{
+    let url = "http://cube.rider.biz/visualcube.php?&view=plan&fmt=png&bg=t&size=1024&stage=oll&case="+encodeURIComponent(alg.replace(/( |\(|\))/g, ''))
+    open(url)
+    console.log(url)
+})
